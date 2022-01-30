74afaae5a580a588214cbc49572f670c168d4203
Juno <jvmeifert@gmail.com>
2022-01-30 17:12:06 -0500

    Added page confirmation for server

--- pager-server.py
+++ pager-server.py
@@ -13,14 +13,26 @@ IMAP_SERVER = "outlook.office365.com"
 IMAP_PORT = 993
 IMAP_PASSWORD = ""
 
+# SMTP login information
+SMTP_ADDR = ""
+SMTP_SERVER = "smtp.office365.com"
+SMTP_PORT = 587
+SMTP_PASSWORD = ""
+
 # Packet radio address of paging server ("xxx.xxx", default "0.0")
 SOURCE_ADDRESS = "0.0"
 
 # Page cooldown in seconds
-PAGE_COOLDOWN = 8
+PAGE_COOLDOWN = 10
 
 # Whitelist for incoming pages
 IMAP_WHITELIST = []
+
+# Subject for outgoing messages
+OUTGOING_MESSAGE_SUBJECT = "Mercury Pager - Page Sent."
+
+# Header for outgoing messages
+OUTGOING_MESSAGE_HEADER = "Thank you for using Mercury Pager.\n" # Requires newline unless empty.
 
 # Utils
 def getDateAndTime(): # Long date and time
@@ -84,26 +96,51 @@ class IMAP:
         lm = self.getMessageCount()
         return self.read(lm, remove)
 
+class SMTP:
+    def __init__(self, useremail, password, server, port):
+        self.useremail = useremail
+        self.password = password
+        self.server = server
+        self.port = port
+
+    def send(self, recipient, subject, message):
+        self.smtp = smtplib.SMTP(self.server, self.port)
+        self.smtp.ehlo()
+        self.smtp.starttls()
+        self.smtp.login(self.useremail, self.password)
+
+        msg = MIMEText(message)
+        msg['Subject'] = subject
+        msg['From'] = self.useremail
+        msg['To'] = recipient
+        self.smtp.sendmail(self.useremail, recipient, msg.as_string())
+
+        self.smtp.quit()
+
 # Main loop
 print("Mercury Pager Server")
+
 im = IMAP(IMAP_ADDR, IMAP_PASSWORD, IMAP_SERVER, IMAP_PORT)
+sm = SMTP(SMTP_ADDR, SMTP_PASSWORD, SMTP_SERVER, SMTP_PORT)
 octs = FormatUtils.parseOctets(SOURCE_ADDRESS)
 ni = NetworkInterface(octs[0], octs[1])
+
 while(True):
     try:
         if(im.getMessageCount() > 0):
             # Fetch mail
             From, Subject, Body = im.readLast(remove=True)
-            if(From in IMAP_WHITELIST): # Avoid spam
-                print(getDateAndTime() + ": Page command received. Sending...")
+            if(From in IMAP_WHITELIST):
+                print(getDateAndTime() + ": Message received from " + From + ".")
                 # Read address from subject
                 destAddr = FormatUtils.parseOctets(Subject)
                 # Assemble packet
                 sp = ni.makePacket(Body.encode("ascii", "ignore"), destAddr[0], destAddr[1])
                 # Send packet
                 ni.sendPacket(sp)
+                print(getDateAndTime() + ": Sent page:\n" + Body + "\nto address " + str(destAddr[0]) + "." + str(destAddr[1]) + ".")
+                sm.send(From, OUTGOING_MESSAGE_SUBJECT, (OUTGOING_MESSAGE_HEADER + "The following page:\n" + Body + "\nto address " + str(destAddr[0]) + "." + str(destAddr[1]) + " was successfully sent on " + getDateAndTime() + "."))
                 # Cool down
-                print(getDateAndTime() + ": Sent message:\n" + Body + "\nto address " + str(destAddr[0]) + "." + str(destAddr[1]) + ".")
                 sleep(PAGE_COOLDOWN)
 
     except Exception as e:
